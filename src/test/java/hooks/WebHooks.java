17df98625a1d59304ca955fa4f9a645ec56273b2
Soloviev Sergey <sol_sergey@bk.ru>
2024-03-21 13:29:25 +0500

    IF_HW_6 init4

--- src/test/java/hooks/WebHooks.java
+++ src/test/java/hooks/WebHooks.java
@@ -3,8 +3,6 @@ package hooks;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -27,12 +25,11 @@ public class WebHooks {
         JiraAuthorizationPage authorizationPage = new JiraAuthorizationPage();
         authorizationPage.login(ConfigProvider.USERNAME, ConfigProvider.PASSWORD);
 
-        SelenideLogger.addListener("AllureSelenide",
+        /*SelenideLogger.addListener("AllureSelenide",
                 new AllureSelenide().
                         screenshots(true).
                         savePageSource(true)
-        );
-
+        );*/
     }
     @AfterEach
     public void tearDown() {
